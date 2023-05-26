8ac3dccd6f5cc81fb8d87275a5e5c75bb69a8386
Shivam6209 <shivammasaistudent@gmail.com>
2023-05-26 17:19:11 +0530

    folder strucutre corrected

--- gymSoftwareSystem/src/app/app.module.ts
+++ gymSoftwareSystem/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './dashboard/dashboard.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
--- gymSoftwareSystem/src/app/dashboard/dashboard.component.ts
+++ gymSoftwareSystem/src/app/dashboard/dashboard.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class AppComponent {
   title = 'gymSoftwareSystem';
